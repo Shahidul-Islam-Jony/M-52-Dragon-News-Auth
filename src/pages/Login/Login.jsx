5f95839b8399595f3d507f6fbfe97c14761e0a41
Shahidul-Islam <shahidulislam13sf@gmail.com>
2023-10-04 00:48:52 +0600

    M52_5-2 Register page with form data to collect data

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,11 +1,49 @@
+import { Link } from "react-router-dom";
 import Navbar from "../Shared/Navbar/Navbar";
 
 
 const Login = () => {
+
+    const handleLogin = e =>{
+        e.preventDefault();
+        // const email = e.target.email.vallue;
+        // const password = e.target.password.value;
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+
+    }
+
     return (
         <div>
             <Navbar></Navbar>
-            <h2 className="text-3xl">This is login</h2>
+            <div>
+                <h2 className="text-3xl my-10 text-center">Please Login</h2>
+
+                <form onSubmit={handleLogin} className="md:3/4 lg:w-1/2 mx-auto" >
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" required placeholder="email" className="input input-bordered"/>
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" required placeholder="password" className="input input-bordered"/>
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Login</button>
+                    </div>
+                </form>
+
+                <p className="text-center mt-4">Do not have an account <Link to='/register' className="text-red-500 font-bold">Register</Link></p>
+
+            </div>
         </div>
     );
 };
---
+++ src/pages/Regsiter/Regsiter.jsx
@@ -0,0 +1,62 @@
+import { Link } from "react-router-dom";
+import Navbar from "../Shared/Navbar/Navbar";
+
+
+const Regsiter = () => {
+
+    const handleRegister = e =>{
+        e.preventDefault();
+        // const email = e.target.email.vallue;
+        // const password = e.target.password.value;
+        console.log(e.currentTarget);
+        const form = new FormData(e.currentTarget);
+        console.log(form.get('email'));
+    }
+
+    return (
+        <div>
+            <Navbar></Navbar>
+            <div>
+                <h2 className="text-3xl my-10 text-center">Please Register</h2>
+
+                <form onSubmit={handleRegister} className="md:3/4 lg:w-1/2 mx-auto" >
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Name</span>
+                        </label>
+                        <input type="text" name="name" required placeholder="Your name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Photo URL</span>
+                        </label>
+                        <input type="text" name="photo" required placeholder="Photo URL" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Email</span>
+                        </label>
+                        <input type="email" name="email" required placeholder="Your email" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text">Password</span>
+                        </label>
+                        <input type="password" name="password" required placeholder="password" className="input input-bordered" />
+                        <label className="label">
+                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                        </label>
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn btn-primary">Register</button>
+                    </div>
+                </form>
+
+                <p className="text-center mt-4">Already have an account <Link to='/login' className="text-blue-500 font-bold">Login</Link></p>
+
+            </div>
+        </div>
+    );
+};
+
+export default Regsiter;
